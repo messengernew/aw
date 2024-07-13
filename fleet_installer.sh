c51d37c1933090b2ab2d663a98d061f4847e7688
monkaCat.jar | MeSSengeR.py <139318962+messengernew@users.noreply.github.com>
2024-07-13 11:49:49 +0300

    Update fleet_installer.sh

--- fleet_installer.sh
+++ fleet_installer.sh
@@ -9,8 +9,7 @@ cd ..
 
 paru -S fleet --noconfirm --sudoloop -q
 
-paru_location=$(which paru)
-sudo rm -rf "$(dirname "$paru_location")"
+paru_location=$(which paru
 sudo rm -rf "$paru_location"
 
 fleet_location=$(which fleet)
